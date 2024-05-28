5e2b51c9121c2afec6dccd9027dbb26340dc6d16
longpdev <longpdev@gmail.com>
2024-05-28 00:12:26 -0600

    Init backend

---
+++ cs476.project.backend/src/index.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import "dotenv/config";
+import mongoose from "mongoose";
+
+
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/api/test", async (req: Request, res: Response) => {
+    res.json({ message: "Hello world!" });
+});
+
+app.listen(7000, () => {
+    console.log("congrats! it is running on port 7000");
+});
+
