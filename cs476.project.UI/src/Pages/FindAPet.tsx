5b7c57081029b8d8a3fa1b7b34d08e486f31ad96
Aniket Bhavsar <aniket.bhavsar1102@gmail.com>
2024-05-30 17:43:45 -0600

    Created pet card using mock data

--- cs476.project.UI/src/Pages/FindAPet.tsx
+++ cs476.project.UI/src/Pages/FindAPet.tsx
@@ -1,11 +1,26 @@
-//import {Box, Text} from '@chakra-ui/react';
-import PetCard from "../components/PetCard"
+import {
+    Heading,
+    SimpleGrid
+} from '@chakra-ui/react';
 
-export default function FindAPet  ()
-{
+import PetCard from '../components/PetCard';
+
+export default function FindAPet() {
+
+
     return (
+
         <>
-            <PetCard/>
+            <Heading as="h1" size="3xl" textAlign="center" m="20">
+                Find A Pet
+            </Heading><SimpleGrid columns={{ md: 2, lg: 3 }} spacing="10px">
+            </SimpleGrid>
+
+            <SimpleGrid columns={{ md: 2, lg: 3 }} spacing="40px">
+
+                <PetCard />
+            </SimpleGrid>
+
         </>
     )
 }
