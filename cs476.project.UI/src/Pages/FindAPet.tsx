e627a73709e23b71a888fa1407242854adcec378
Aniket Bhavsar <aniket.bhavsar1102@gmail.com>
2024-05-29 18:24:06 -0600

    Added navbar & linked all the pages through navbar

---
+++ cs476.project.UI/src/Pages/FindAPet.tsx
@@ -0,0 +1,12 @@
+import {Box, Text} from '@chakra-ui/react';
+
+export default function FindAPet  ()
+{
+    return (
+        <>
+            <Box bg='yellow' w='100%' h='100%' p={4} color='red'>
+                <Text> This is FindAPet page </Text>
+            </Box>
+        </>
+    )
+}
