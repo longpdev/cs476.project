eaf903813faffae0a3f86cdb025686c829b8e347
Aniket Bhavsar <aniket.bhavsar1102@gmail.com>
2024-06-03 17:44:20 -0600

    Updated petAdvice page

--- cs476.project.UI/src/Pages/PetAdvice.tsx
+++ cs476.project.UI/src/Pages/PetAdvice.tsx
@@ -1,12 +1,57 @@
-import {Box, Text} from '@chakra-ui/react';
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
 
-export default function PetAdvice ()
-{
-    return (
-        <>
-            <Box bg='yellow' w='100%' p={4} color='red'>
-                <Text> This is Home Page </Text>
-            </Box>
-        </>
-    )
+import { useState, useEffect } from "react";
+
+import petAdviceFaqsData from "../assets/PetAdviceFaqs.json";
+
+type PetAdviceFaqs = {
+  question: string;
+  details: string;
+};
+
+export default function PetAdvice() {
+  const [petAdvicefaqs, setpetAdviceFaqs] = useState<PetAdviceFaqs[]>([]);
+
+  useEffect(() => {
+    return setpetAdviceFaqs(petAdviceFaqsData.petAdvicefaqs);
+  }, []);
+
+  return (
+    <>
+      <Heading as="h1" size="3xl" textAlign="center" m="20">
+        Pet Advice
+      </Heading>
+
+      <Text as="h1" size="lg" textAlign="center" m="20">
+        Consider them before adopting a pet
+      </Text>
+
+      <Box p="10">
+        {petAdvicefaqs.map((petAdvicefaqs) => (
+          <Accordion allowMultiple>
+            <AccordionItem>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                    <Text as="b">{petAdvicefaqs.question}</Text>
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>{petAdvicefaqs.details}</AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+        ))}
+      </Box>
+    </>
+  );
 }
