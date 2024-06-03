2e6e131849a61d7b7318d007650c163b90b66a91
aabhushan <aabhu57shan@gmail.com>
2024-06-02 21:11:40 -0600

    added fields on userSchema (Registeration)

--- cs476.project.backend/src/models/user.ts
+++ cs476.project.backend/src/models/user.ts
@@ -1,17 +1,22 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 type UserType = {
-    _id: string;
-    email: string;
-    password: string;
-}
+  _id: string;
+  email: string;
+  password: string;
+  fullName: string;
+  postalCode: string;
+  contactNumber: number;
+};
 
 const userSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true},
-    password: { type: String, required: true },
-    createdDate: { type: Date, default: Date.now, required: false },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  fullName: { type: String },
+  postalCode: { type: String },
+  contactNumber: { type: Number, unique: true },
+  createdDate: { type: Date, default: Date.now, required: false },
 });
-
 
 const UserModel = mongoose.model<UserType>("User", userSchema);
 
