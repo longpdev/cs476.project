3dce5ced3b1ff7900be7323ed1987c3f555dd579
aabhushan <aabhu57shan@gmail.com>
2024-06-02 21:59:24 -0600

    set up nodemailer for emails

--- cs476.project.backend/src/index.ts
+++ cs476.project.backend/src/index.ts
@@ -1,9 +1,9 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
-
+import sendEmail from "./utils/emailSender";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -15,10 +15,15 @@ app.use(cors());
 app.use("/api/users", userRoutes);
 
 app.get("/api/test", async (req: Request, res: Response) => {
-    res.json({ message: "Hello world!" });
+  //res.json({ message: "Hello world!" });
+  return sendEmail(
+    "akoiralaaa@gmail.com",
+    "petAdoption@gmail.com",
+    "Test Email",
+    "This is a test email sent using Nodemailer."
+  );
 });
 
-
 app.listen(3000, () => {
-    console.log("congrats! it is running on port 3000");
+  console.log("congrats! it is running on port 3000");
 });
