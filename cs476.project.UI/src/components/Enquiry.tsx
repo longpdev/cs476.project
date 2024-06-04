0f41d14a78050ee3f7c562fe3421b19d5dc6310f
aabhushan <aabhu57shan@gmail.com>
2024-06-03 23:03:53 -0600

    Set up enquiry form. Works fine for now. Give it  try.

--- cs476.project.UI/src/components/Enquiry.tsx
+++ cs476.project.UI/src/components/Enquiry.tsx
@@ -1,62 +1,56 @@
-import React from 'react';
-import {
-  Box,
-  Button,
-  FormControl,
-  FormLabel,
-  Input,
-  Textarea,
-  VStack,
-} from '@chakra-ui/react';
+// import React from 'react';
+// import {
+//   Box,
+//   Button,
+//   FormControl,
+//   FormLabel,
+//   Input,
+//   Textarea,
+//   VStack,
+// } from '@chakra-ui/react';
 
+// export default function Enquiry ()
+// {
+//     return (
 
+//     <Box maxW="md" mx="auto" mt={10} p={6} borderWidth={1} borderRadius="lg" boxShadow="lg">
 
-export default function Enquiry ()
-{
-    return (
+//       <form >
+//         <VStack spacing={4}>
+//           <FormControl id="name" isRequired>
+//             <FormLabel>Name</FormLabel>
+//             <Input
+//               type="text"
 
-  
-    <Box maxW="md" mx="auto" mt={10} p={6} borderWidth={1} borderRadius="lg" boxShadow="lg">
-      
-      <form >
-        <VStack spacing={4}>
-          <FormControl id="name" isRequired>
-            <FormLabel>Name</FormLabel>
-            <Input
-              type="text"
-              
-             
-            />
-          </FormControl>
-          <FormControl id="email" isRequired>
-            <FormLabel>Email</FormLabel>
-            <Input
-              type="email"
-              
-              
-            />
-          </FormControl>
-          <FormControl id="subject" isRequired>
-            <FormLabel>Subject</FormLabel>
-            <Input
-              type="text"
-              
-            />
-          </FormControl>
-          <FormControl id="message" isRequired>
-            <FormLabel>Message</FormLabel>
-            <Textarea
-              
-            />
-          </FormControl>
-          <Button colorScheme="teal" type="submit" width="full">
-            Submit
-          </Button>
-        </VStack>
-      </form>
-    </Box>
-  );
-}
+//             />
+//           </FormControl>
+//           <FormControl id="email" isRequired>
+//             <FormLabel>Email</FormLabel>
+//             <Input
+//               type="email"
 
+//             />
+//           </FormControl>
+//           <FormControl id="subject" isRequired>
+//             <FormLabel>Subject</FormLabel>
+//             <Input
+//               type="text"
 
-//export default Enquiry;
+//             />
+//           </FormControl>
+//           <FormControl id="message" isRequired>
+//             <FormLabel>Message</FormLabel>
+//             <Textarea
+
+//             />
+//           </FormControl>
+//           <Button colorScheme="teal" type="submit" width="full">
+//             Submit
+//           </Button>
+//         </VStack>
+//       </form>
+//     </Box>
+//   );
+// }
+
+// //export default Enquiry;
