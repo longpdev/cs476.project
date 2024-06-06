20a58f7c57c8d5d6f09df35529a119f10aec38cb
Aniket Bhavsar <aniket.bhavsar1102@gmail.com>
2024-06-05 19:06:46 -0600

    Fixed bug related to App being refreshed when navbar links are clicked

--- cs476.project.UI/src/components/NavbarItems.tsx
+++ cs476.project.UI/src/components/NavbarItems.tsx
@@ -1,51 +1,92 @@
-import { Link, Stack, Image, useBreakpointValue, Spacer } from '@chakra-ui/react';
-import { ProfileMenu } from './ProfileMenu';
+import { Link as RouterLink } from "react-router-dom";
+import {
+  Stack,
+  Image,
+  useBreakpointValue,
+  Spacer,
+  Link,
+} from "@chakra-ui/react";
+import { ProfileMenu } from "./ProfileMenu";
 
 export const NavbarItems = () => {
+  const isMobile = useBreakpointValue({ base: true, lg: false });
 
-    const isMobile = useBreakpointValue({ base: true, lg: false });
+  return (
+    <>
+      <Stack
+        direction={{ base: "column", lg: "row" }}
+        as="nav"
+        spacing={{ base: 2, lg: 3 }}
+        alignItems={{ base: "left", lg: "center" }}
+      >
+        {!isMobile && (
+          <Image
+            src="src/assets/logo-image-circle.png"
+            alt="Logo"
+            style={{ height: "80px" }}
+            borderRadius={"50%"}
+          />
+        )}
+        <Link
+          as={RouterLink}
+          to="/"
+          px={2}
+          py={1}
+          rounded="md"
+          textDecoration="none"
+          _hover={{ bg: "teal.800" }}
+        >
+          Home
+        </Link>
+        <Link
+          as={RouterLink}
+          to="/FindAPet"
+          px={2}
+          py={1}
+          rounded="md"
+          textDecoration="none"
+          _hover={{ bg: "teal.800" }}
+        >
+          Find a Pet
+        </Link>
+        <Link
+          as={RouterLink}
+          to="/PetAdvice"
+          px={2}
+          py={1}
+          rounded="md"
+          textDecoration="none"
+          _hover={{ bg: "teal.800" }}
+        >
+          Pet Advice
+        </Link>
+        <Link
+          as={RouterLink}
+          to="/MyAdoptions"
+          px={2}
+          py={1}
+          rounded="md"
+          textDecoration="none"
+          _hover={{ bg: "teal.800" }}
+        >
+          My Adoptions
+        </Link>
+        <Link
+          as={RouterLink}
+          to="/AboutUs"
+          px={2}
+          py={1}
+          rounded="md"
+          textDecoration="none"
+          _hover={{ bg: "teal.800" }}
+        >
+          About Us
+        </Link>
 
-    return (
-        <>
-            <Stack direction={{ base: 'column', lg: 'row' }} as="nav" spacing={{ base: 2, lg: 3 }} alignItems={{ base: 'left', lg: 'center' }}>
-                {!isMobile && (<Image src="src\assets\logo-image-circle.png" alt="Logo" style={{ height: '80px' }} borderRadius={"50%"} />)}
-                <Link
-                    px={2}
-                    py={1}
-                    size={'10px'}
-                    rounded="md"
-                    textDecoration="none"
-                    _hover={{ bg: 'teal.800' }}
-                    href="/"
-                >
-                    Home
-                </Link>
-                <Link px={2} py={1} rounded="md" _hover={{ bg: 'teal.800' }} href="/FindAPet" textDecoration="none">
-                    Find a Pet
+        <Spacer />
 
-                </Link>
-                <Link px={2} py={1} rounded="md" textDecoration="none" _hover={{ bg: 'teal.800' }} href="/PetAdvice">
-                    Pet Advice
-                </Link>
-                <Link
-                    px={2}
-                    py={1}
-                    size={'1px'}
-                    rounded="md"
-                    textDecoration="none"
-                    _hover={{ bg: 'teal.800' }}
-                    href="/MyAdoptions"
-                >
-                    My Adoptions
-                </Link>
-                <Link px={2} py={1} rounded="md" textDecoration="none" _hover={{ bg: 'teal.800' }} href="/AboutUs">
-                    About Us
-                </Link>
-
-                <Spacer />
-
-                {!isMobile && (<ProfileMenu />)}
-            </Stack>
-        </>
-    )
+        {!isMobile && <ProfileMenu />}
+      </Stack>
+    </>
+  );
 };
