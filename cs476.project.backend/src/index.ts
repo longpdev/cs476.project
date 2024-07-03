a907fef6a9ac1e2e1aff701c9b314ae9493c3121
longpdev <longpdev@gmail.com>
2024-07-02 21:58:02 -0600

    Setup cloudinary

--- cs476.project.backend/src/index.ts
+++ cs476.project.backend/src/index.ts
@@ -1,10 +1,17 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import { v2 as cloudinary } from 'cloudinary';
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import userRoutes from './routes/users';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
