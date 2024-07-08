233894b426dfb057978df55dc135b7e0da856ecd
Aniket Bhavsar <aniket.bhavsar1102@gmail.com>
2024-07-08 15:39:27 -0600

    fixed pipeline issue

--- cs476.project.UI/src/components/homePageCarousel.tsx
+++ cs476.project.UI/src/components/homePageCarousel.tsx
@@ -1,6 +1,5 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import "../assets/homePageCarousel.css";
 
 export const HomePageCarousel = () => {
   return (
