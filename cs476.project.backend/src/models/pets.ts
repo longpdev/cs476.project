3f8314e7ea4657f66e8005d91f8c5b97380ef266
Aniket Bhavsar <aniket.bhavsar1102@gmail.com>
2024-07-08 16:26:48 -0600

    Updated Pet Model

--- cs476.project.backend/src/models/pets.ts
+++ cs476.project.backend/src/models/pets.ts
@@ -4,6 +4,17 @@ export type PetType = {
   name: string;
   breed: string;
   imageURLs: string[];
+  age: string;
+  sex: string;
+  category: string;
+  description: string;
+  trained: string;
+  characterstics: string;
+  health: string;
+  colour: string;
+  height: string;
+  weight: string;
+  accommodative: string;
 };
 
 const petSchema = new mongoose.Schema({
@@ -11,6 +22,17 @@ const petSchema = new mongoose.Schema({
   breed: { type: String, required: true },
   imageURLs: { type: [String], required: true },
   createdDate: { type: Date, default: Date.now, required: false },
+  age: { type: String, required: true },
+  sex: { type: String, required: true },
+  category: { type: String, required: true },
+  description: { type: String, required: true },
+  trained: { type: String, required: true },
+  characterstics: { type: String, required: true },
+  health: { type: String, required: true },
+  colour: { type: String, required: true },
+  height: { type: String, required: true },
+  weight: { type: String, required: true },
+  accommodative: { type: String, required: true },
 });
 
 const Pet = mongoose.model<PetType>('Pet', petSchema);
