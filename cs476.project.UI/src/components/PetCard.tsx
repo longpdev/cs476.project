91461405ec4a888a1193b806e1669c43e8149b3f
LongPham <113424246+longpdev@users.noreply.github.com>
2024-07-11 09:31:31 -0600

    Revert "Aniket update pet model"

--- cs476.project.UI/src/components/PetCard.tsx
+++ cs476.project.UI/src/components/PetCard.tsx
@@ -10,75 +10,47 @@ import {
   TableContainer,
   Image,
   Heading,
-  Button,
 } from '@chakra-ui/react';
 import { PetType } from '../Pages/FindAPet';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 type PetCardProps = {
-  pets: PetType[];
+  pet: PetType;
 };
 
-export default function PetCard({ pets }: PetCardProps) {
-  const navigate = useNavigate();
-
-  const handleViewDetail = (_id: string) => {
-    navigate(`/pet/${_id}`);
-  };
+export default function PetCard({ pet }: PetCardProps) {
+  console.log('Pet:', pet._id);
   return (
     <>
-      {pets.map((pet) => (
-        <Card maxW="sm" key={pet._id}>
-          <CardBody>
-            <Image
-              width={'100%'}
-              src={pet.imageURLs[0]}
-              alt={pet.name}
-              borderRadius="lg"
-            />
-            <Stack mt="6" spacing="1">
-              <Heading textAlign={'center'} as="b" size="lg" mb="6">
-                {pet.name}
-              </Heading>
-              <TableContainer m="0px">
-                <Table variant="striped" colorScheme="teal">
-                  <Tbody>
-                    <Tr>
-                      <Td>
-                        {' '}
-                        <Text as="b">Breed : </Text>
-                        {pet.breed}
-                      </Td>
-                    </Tr>
-                    <Tr>
-                      <Td>
-                        {' '}
-                        <Text as="b"> Age : </Text>
-                        {pet.age}
-                      </Td>
-                    </Tr>
-                    <Tr>
-                      <Td>
-                        {' '}
-                        <Text as="b"> Sex : </Text>
-                        {pet.sex}
-                      </Td>
-                    </Tr>
-                  </Tbody>
-                </Table>
-              </TableContainer>
-              <Button
-                onClick={() => handleViewDetail(pet._id)}
-                variant="solid"
-                colorScheme="blue"
-                mt={'6'}
-              >
-                View Detail
-              </Button>
-            </Stack>
-          </CardBody>
-        </Card>
-      ))}
+      <Card maxW="sm" key={pet._id}>
+        <CardBody>
+          <Image
+            width={'100%'}
+            src={pet.imageURLs[0]}
+            alt="Green double couch with wooden legs"
+            borderRadius="lg"
+          />
+          <Stack mt="6" spacing="1">
+            <Heading textAlign={'center'} as="b" size="lg" mb="6">
+              {pet.name}
+            </Heading>
+            <TableContainer m="0px">
+              <Table variant="striped" colorScheme="teal">
+                <Tbody>
+                  <Tr>
+                    <Td>
+                      {' '}
+                      <Text as="b">Breed : </Text>
+                      {pet.breed}
+                    </Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </TableContainer>
+            <Link to={`/editPet/${pet._id}`}>View Detail</Link>
+          </Stack>
+        </CardBody>
+      </Card>
     </>
   );
 }
