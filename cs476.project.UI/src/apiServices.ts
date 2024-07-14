f9da8a228e4edf29d43e0ab1ce37a2f40efa7e16
aabhushan <aabhu57shan@gmail.com>
2024-07-14 12:53:18 -0600

    total user count added on admin dashboard

--- cs476.project.UI/src/apiServices.ts
+++ cs476.project.UI/src/apiServices.ts
@@ -4,139 +4,140 @@ import { PetType } from './Pages/FindAPet';
 const API_URL = import.meta.env.VITE_API_BASE_URL || '';
 
 export const registerAPI = async (data: RegisterData) => {
-  const res = await fetch(`${API_URL}/api/users/register`, {
-    method: 'POST',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  });
+ const res = await fetch(`${API_URL}/api/users/register`, {
+  method: 'POST',
+  credentials: 'include',
+  headers: {
+   'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+ });
 
-  const resBody = await res.json();
-  if (!res.ok) throw new Error(resBody.message || 'Failed to register!');
-  return resBody;
+ const resBody = await res.json();
+ if (!res.ok) throw new Error(resBody.message || 'Failed to register!');
+ return resBody;
 };
 
 export const loginAPI = async (data: LoginData) => {
-  const res = await fetch(`${API_URL}/api/users/login`, {
-    method: 'POST',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  });
+ const res = await fetch(`${API_URL}/api/users/login`, {
+  method: 'POST',
+  credentials: 'include',
+  headers: {
+   'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+ });
 
-  const resBody = await res.json();
-  if (!res.ok) throw new Error(resBody.message || 'Failed to login!');
-  return resBody;
+ const resBody = await res.json();
+ if (!res.ok) throw new Error(resBody.message || 'Failed to login!');
+ return resBody;
 };
 
 export const signoutAPI = async () => {
-  const res = await fetch(`${API_URL}/api/users/signout`, {
-    method: 'POST',
-    credentials: 'include',
-  });
+ const res = await fetch(`${API_URL}/api/users/signout`, {
+  method: 'POST',
+  credentials: 'include',
+ });
 
-  if (!res.ok) throw new Error('Failed to signout!');
-  return res.json();
+ if (!res.ok) throw new Error('Failed to signout!');
+ return res.json();
 };
 
 export const verifyToken = async () => {
-  const res = await fetch(`${API_URL}/api/users/verifytoken`, {
-    method: 'GET',
-    credentials: 'include',
-  });
+ const res = await fetch(`${API_URL}/api/users/verifytoken`, {
+  method: 'GET',
+  credentials: 'include',
+ });
 
-  if (!res.ok) throw new Error('Failed to verify token!');
-  return res.json();
+ if (!res.ok) throw new Error('Failed to verify token!');
+ return res.json();
 };
 
 export const addPetApi = async (petFormData: FormData) => {
-  const res = await fetch(`${API_URL}/api/pets/addpet`, {
-    method: 'POST',
-    credentials: 'include',
-    body: petFormData,
-  });
+ const res = await fetch(`${API_URL}/api/pets/addpet`, {
+  method: 'POST',
+  credentials: 'include',
+  body: petFormData,
+ });
 
-  if (!res.ok) throw new Error('Failed to add pet!');
-  return res.json();
+ if (!res.ok) throw new Error('Failed to add pet!');
+ return res.json();
 };
 
 export const getAllPets = async (): Promise<PetType[]> => {
-  const res = await fetch(`${API_URL}/api/pets`);
+ const res = await fetch(`${API_URL}/api/pets`);
 
-  if (!res.ok) throw new Error('Failed to get all pets');
-  return res.json();
+ if (!res.ok) throw new Error('Failed to get all pets');
+ return res.json();
 };
 
 export const fetchPetById = async (id: string): Promise<PetType> => {
-  const res = await fetch(`${API_URL}/api/pets/getpet/${id}`);
-  if (!res.ok) throw new Error('Failed to get pet by id');
-  return res.json();
+ const res = await fetch(`${API_URL}/api/pets/getpet/${id}`);
+ if (!res.ok) throw new Error('Failed to get pet by id');
+ return res.json();
 };
 
 export const updatePetById = async (petFormData: FormData) => {
-  const res = await fetch(
-    `${API_URL}/api/pets/updatepet/${petFormData.get('_id')}`,
-    {
-      method: 'PUT',
-      credentials: 'include',
-      body: petFormData,
-    }
-  );
+ const res = await fetch(
+  `${API_URL}/api/pets/updatepet/${petFormData.get('_id')}`,
+  {
+   method: 'PUT',
+   credentials: 'include',
+   body: petFormData,
+  }
+ );
 
-  if (!res.ok) throw new Error('Failed to update pet!');
-  return res.json();
+ if (!res.ok) throw new Error('Failed to update pet!');
+ return res.json();
 };
 
 export const deletePetById = async (id: string) => {
-  const res = await fetch(`${API_URL}/api/pets/deletePet/${id}`, {
-    method: 'DELETE',
-    credentials: 'include',
-  });
+ const res = await fetch(`${API_URL}/api/pets/deletePet/${id}`, {
+  method: 'DELETE',
+  credentials: 'include',
+ });
 
-  if (!res.ok) throw new Error('Failed to delete pet by Id!');
-  return res.json();
+ if (!res.ok) throw new Error('Failed to delete pet by Id!');
+ return res.json();
 };
 
-export const getAllUser = async () => {
-  const res = await fetch(`${API_URL}/api/users/getalluser`, {
-    method: 'GET',
-    credentials: 'include',
-  });
+//USERS//
 
-  if (!res.ok) throw new Error('Failed to fetch user!');
-  return res.json();
+export const getAllUser = async () => {
+ const res = await fetch(`${API_URL}/api/users/getalluser`, {
+  method: 'GET',
+  credentials: 'include',
+ });
+
+ if (!res.ok) throw new Error('Failed to fetch user!');
+ return res.json();
 };
 
 //update user
 export const updateUser = async (props: {
-  id: string;
-  userDetail: {
-    email: string;
-    firstName: string;
-    lastName: string;
-    phoneNumber: string;
-  };
+ id: string;
+ userDetail: {
+  email: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+ };
 }) => {
-  const response = await fetch(`${API_URL}/api/users/updateuser/${props.id}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'Application/json',
-    },
-    body: JSON.stringify(props.userDetail),
-  });
-  return response.json;
+ const response = await fetch(`${API_URL}/api/users/updateuser/${props.id}`, {
+  method: 'PUT',
+  headers: {
+   'Content-Type': 'Application/json',
+  },
+  body: JSON.stringify(props.userDetail),
+ });
+ return response.json;
 };
 
 //delete user
 export const deleteUser = async (id: string) => {
-  const response = await fetch(`${API_URL}/api/users/${id}`, {
-    method: 'DELETE',
-  });
-  if (!response.ok) throw new Error('Failed to delete user!');
-  return response.json();
+ const response = await fetch(`${API_URL}/api/users/${id}`, {
+  method: 'DELETE',
+ });
+ if (!response.ok) throw new Error('Failed to delete user!');
+ return response.json();
 };
-
