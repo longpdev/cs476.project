378e4aa2029341c352aa7b7058054484c2b2856a
Aniket Bhavsar <aniket.bhavsar1102@gmail.com>
2024-07-19 12:48:58 -0600

    Updated Profile menu icon to change according to user's first name

--- cs476.project.UI/src/components/ProfileMenu.tsx
+++ cs476.project.UI/src/components/ProfileMenu.tsx
@@ -1,36 +1,125 @@
+import React, { useState, useEffect } from 'react';
 import {
-  Avatar,
   Menu,
   MenuButton,
   MenuList,
   MenuItem,
   MenuGroup,
-  Button,
+  Box,
 } from '@chakra-ui/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import {
+  faA,
+  faB,
+  faC,
+  faD,
+  faE,
+  faF,
+  faG,
+  faH,
+  faI,
+  faJ,
+  faK,
+  faL,
+  faM,
+  faN,
+  faO,
+  faP,
+  faQ,
+  faR,
+  faS,
+  faT,
+  faU,
+  faV,
+  faW,
+  faX,
+  faY,
+  faZ,
+} from '@fortawesome/free-solid-svg-icons';
+import { useQuery } from 'react-query';
+import { getAllUser } from '../apiServices';
+import { useAppContext } from '../contexts/AppContext';
 import { SignOut } from './SignOut';
 
-export const ProfileMenu = () => {
+const iconMap: { [key: string]: IconDefinition } = {
+  A: faA,
+  B: faB,
+  C: faC,
+  D: faD,
+  E: faE,
+  F: faF,
+  G: faG,
+  H: faH,
+  I: faI,
+  J: faJ,
+  K: faK,
+  L: faL,
+  M: faM,
+  N: faN,
+  O: faO,
+  P: faP,
+  Q: faQ,
+  R: faR,
+  S: faS,
+  T: faT,
+  U: faU,
+  V: faV,
+  W: faW,
+  X: faX,
+  Y: faY,
+  Z: faZ,
+};
+
+interface UserType {
+  _id: string;
+  firstName: string;
+}
+
+export const ProfileMenu: React.FC = () => {
+  const { userId } = useAppContext();
+  const { data: users } = useQuery({
+    queryKey: ['users'],
+    queryFn: getAllUser,
+    refetchOnWindowFocus: false,
+  });
+
+  const [initial, setInitial] = useState<string>('A');
+
+  useEffect(() => {
+    if (users) {
+      const currentUser = users.find((user: UserType) => user._id === userId);
+      if (currentUser && currentUser.firstName) {
+        setInitial(currentUser.firstName.charAt(0).toUpperCase());
+      }
+    }
+  }, [users, userId]);
+
   return (
-    <>
-      <Menu>
-        <MenuButton borderRadius={50} m={2} _hover={{ bg: 'teal.800' }}>
-          <Avatar
-            size="md"
-            name="Christian Nwamba"
-            src="https://bit.ly/code-beast"
-          />
-        </MenuButton>
-        <MenuList>
-          <MenuGroup title="Profile">
-            <MenuItem>
-              <SignOut />
-            </MenuItem>
-            <MenuItem as="a" href="/MyAccount">
-              <Button>My Account</Button>
-            </MenuItem>
-          </MenuGroup>
-        </MenuList>
-      </Menu>
-    </>
+    <Menu>
+      <MenuButton borderRadius={50} m={2}>
+        <Box
+          backgroundColor="teal.800"
+          borderRadius="100%"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          width="50px"
+          height="50px"
+        >
+          <FontAwesomeIcon color="white" icon={iconMap[initial]} size="xl" />
+        </Box>
+      </MenuButton>
+      <MenuList>
+        <MenuGroup title="Profile">
+          <MenuItem as="a" href="/MyAccount">
+            My Account
+          </MenuItem>
+          <MenuItem>
+            <SignOut />
+          </MenuItem>
+        </MenuGroup>
+      </MenuList>
+    </Menu>
   );
 };
