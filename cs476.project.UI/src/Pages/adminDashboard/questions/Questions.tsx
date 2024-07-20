ec81ecb7fc54058a8318de14dceea5950f5cc4e1
longtran21700 <68406135+longtran21700@users.noreply.github.com>
2024-07-20 02:50:27 -0600

    Display application info on dashboard

--- cs476.project.UI/src/Pages/adminDashboard/questions/Questions.tsx
+++ cs476.project.UI/src/Pages/adminDashboard/questions/Questions.tsx
@@ -1,10 +1,46 @@
 import { useQuery } from 'react-query';
 import { getAllQuestions } from '../../../apiServices';
 
-import { Text } from '@chakra-ui/react';
+import {
+  Button,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
 
 export const Questions = () => {
-  const { data } = useQuery('questions', getAllQuestions);
-  console.log(data);
-  return <Text>Test</Text>;
+  const { data: questions } = useQuery('questions', getAllQuestions);
+
+  console.log(questions);
+  return (
+    <Table variant="simple">
+      <Thead>
+        <Tr>
+          <Th>First Name</Th>
+          <Th>Last Name</Th>
+          <Th>Phone Number</Th>
+          <Th>Home Type</Th>
+          <Th>Inspection Date</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {questions?.map((question: any) => (
+          <Tr key={question._id}>
+            <Td>{question.firstName}</Td>
+            <Td>{question.lastName}</Td>
+            <Td>{question.phoneNumber}</Td>
+            <Td>{question.homeType}</Td>
+            <Td>{question.inspectionDate}</Td>
+            <Td>
+              <Button>View</Button>
+            </Td>
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
+  );
 };
