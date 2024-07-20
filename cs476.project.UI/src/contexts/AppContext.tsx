a9395f5279f6d8439b7743469d55072b4193f36b
longpdev <longpdev@gmail.com>
2024-07-19 23:55:09 -0600

    Return userId via AppContext

--- cs476.project.UI/src/contexts/AppContext.tsx
+++ cs476.project.UI/src/contexts/AppContext.tsx
@@ -1,15 +1,16 @@
-import React, { useContext, ReactNode } from "react";
-import { useToast } from "@chakra-ui/react";
-import { useQuery } from "react-query";
-import { verifyToken } from "../apiServices";
+import React, { useContext, ReactNode, useState, useEffect } from 'react';
+import { useToast } from '@chakra-ui/react';
+import { useQuery } from 'react-query';
+import { verifyToken } from '../apiServices';
 type ToastMessage = {
   message: string;
-  type: "success" | "error";
+  type: 'success' | 'error';
 };
 
 type AppContextType = {
   showToast: (toastMessage: ToastMessage) => void;
   isAuthenticated: boolean;
+  userId: string | null;
 };
 
 const AppContext = React.createContext<AppContextType | undefined>(undefined);
@@ -20,20 +21,29 @@ type AppContextProviderProps = {
 
 export const AppContextProvider = ({ children }: AppContextProviderProps) => {
   const toast = useToast();
-  const { isError } = useQuery("verifytoken", verifyToken, {
+  const { isError, data } = useQuery('verifytoken', verifyToken, {
     retry: false,
   });
+  const [userId, setUserId] = useState<string | null>(null);
+  useEffect(() => {
+    if (data) {
+      setUserId(data.userId);
+    }
+  }, [data]);
+  console.log(data);
   const showToast = ({ message, type }: ToastMessage) => {
     toast({
       title: message,
       status: type,
       duration: 5000,
       isClosable: true,
-      position: "top-right",
+      position: 'top-right',
     });
   };
   return (
-    <AppContext.Provider value={{ showToast, isAuthenticated: !isError }}>
+    <AppContext.Provider
+      value={{ showToast, isAuthenticated: !isError, userId }}
+    >
       {children}
     </AppContext.Provider>
   );
