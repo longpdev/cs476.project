6c9c3b4cd768a08fc8ae0fc32ef8d838953e722d
longpdev <longpdev@gmail.com>
2024-07-21 00:05:48 -0600

    added user object into AppContext, some minor changes

--- cs476.project.UI/src/Pages/Unauthenticated/NotAnAdmin.tsx
+++ cs476.project.UI/src/Pages/Unauthenticated/NotAnAdmin.tsx
@@ -1,5 +1,5 @@
-import { Text } from '@chakra-ui/react';
+import { Heading, Text } from '@chakra-ui/react';
 
 export const NotAnAdmin = () => {
-  return <Text>Not an Admin</Text>;
+  return <Heading>Permission Required!</Heading>;
 };
