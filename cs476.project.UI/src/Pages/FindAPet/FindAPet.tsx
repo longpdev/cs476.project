1ab58056a563cbbddfd65231e36c3e4b201dc29d
longtran21700 <68406135+longtran21700@users.noreply.github.com>
2024-07-20 18:07:34 -0600

    Add a button for "Add Pet"

--- cs476.project.UI/src/Pages/FindAPet/FindAPet.tsx
+++ cs476.project.UI/src/Pages/FindAPet/FindAPet.tsx
@@ -5,12 +5,14 @@ import {
   Input,
   Select,
   Box,
+  Button,
 } from '@chakra-ui/react';
 import { getAllPets } from '../../apiServices';
 import { useQuery } from 'react-query';
 import { useState, useEffect } from 'react';
 import PetCard from '../../components/PetCard';
 import { useAppContext } from '../../contexts/AppContext';
+import { useNavigate } from 'react-router-dom';
 
 export type PetType = {
   _id: string;
@@ -30,6 +32,7 @@ export type PetType = {
 };
 
 export default function FindAPet() {
+  const navigate = useNavigate();
   const { data, isError, isLoading } = useQuery('pets', getAllPets);
   const [pets, setPets] = useState<PetType[]>([]);
   const [searchName, setSearchName] = useState('');
@@ -73,35 +76,50 @@ export default function FindAPet() {
       <Heading as="h1" size="3xl" textAlign="center" m="20">
         Find A Pet
       </Heading>
+      <Box display="flex" flexDirection="column" alignItems="center" mb="10">
+        <Box mb="4">
+          <Button
+            mt={4}
+            colorScheme="red"
+            width="auto"
+            size="lg"
+            onClick={() => {
+              navigate('/AddPet');
+            }}
+          >
+            Are you an admin? Add Pet
+          </Button>
+        </Box>
 
-      <Box display="flex" justifyContent="center" mb="10">
-        <Input
-          placeholder="Search by name"
-          value={searchName}
-          onChange={handleSearchChange}
-          width="300px"
-          mr="10px"
-        />
-        <Select
-          placeholder="Filter by category"
-          value={filterCategory}
-          onChange={handleFilterCategoryChange}
-          width="200px"
-          mr="10px"
-        >
-          <option value="Dog">Dog</option>
-          <option value="Cat">Cat</option>
-          <option value="Bird">Bird</option>
-        </Select>
-        <Select
-          placeholder="Filter by sex"
-          value={filterSex}
-          onChange={handleFilterSexChange}
-          width="150px"
-        >
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-        </Select>
+        <Box display="flex" justifyContent="center" mb="10">
+          <Input
+            placeholder="Search by name"
+            value={searchName}
+            onChange={handleSearchChange}
+            width="300px"
+            mr="10px"
+          />
+          <Select
+            placeholder="Filter by category"
+            value={filterCategory}
+            onChange={handleFilterCategoryChange}
+            width="200px"
+            mr="10px"
+          >
+            <option value="Dog">Dog</option>
+            <option value="Cat">Cat</option>
+            <option value="Bird">Bird</option>
+          </Select>
+          <Select
+            placeholder="Filter by sex"
+            value={filterSex}
+            onChange={handleFilterSexChange}
+            width="150px"
+          >
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </Select>
+        </Box>
       </Box>
 
       <SimpleGrid columns={{ md: 2, lg: 3 }} spacing="40px">
