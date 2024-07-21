7139af577a7c091c46805cbebd6c61c8eaae0484
longpdev <longpdev@gmail.com>
2024-07-21 00:08:09 -0600

    Minor change

--- cs476.project.UI/src/Pages/Unauthenticated/NotAnAdmin.tsx
+++ cs476.project.UI/src/Pages/Unauthenticated/NotAnAdmin.tsx
@@ -1,5 +1,10 @@
-import { Heading, Text } from '@chakra-ui/react';
+import { Box, Heading, Text } from '@chakra-ui/react';
 
 export const NotAnAdmin = () => {
-  return <Heading>Permission Required!</Heading>;
+  return (
+    <Box>
+      <Heading>Permission Required!</Heading>
+      <Text>duh, you need admin permission</Text>
+    </Box>
+  );
 };
