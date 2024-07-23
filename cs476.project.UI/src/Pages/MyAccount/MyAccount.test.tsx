c2c68e62b355018e5355772b64fcba52a48a20d3
longpdev <longpdev@gmail.com>
2024-07-22 23:54:07 -0600

    "Fix pipline complains"

--- cs476.project.UI/src/Pages/MyAccount/MyAccount.test.tsx
+++ cs476.project.UI/src/Pages/MyAccount/MyAccount.test.tsx
@@ -2,9 +2,9 @@ import { it, expect, describe } from 'vitest';
 import MyAccount from './MyAccount';
 import { render, screen } from '@testing-library/react';
 import { AppContextProvider } from '../../contexts/AppContext';
-import { QueryClientProvider } from 'react-query';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import '@testing-library/jest-dom/vitest';
-import { QueryClient } from 'react-query';
+
 describe('My Account', () => {
   it('should render the FAQs page', () => {
     const queryClient = new QueryClient();
